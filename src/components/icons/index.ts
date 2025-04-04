e301bad6d3ea32f243cdd016f2acaf30c8fefb33
Igor Veleski <vele.igor@gmail.com>
2025-04-04 17:48:27 +0200

    remove unnecessary icon components

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,7 +1,5 @@
 import { IconUser } from './IconUser';
-import { IconBell } from './IconBell';
 import { IconBriefcase } from './IconBriefcase';
-import { IconBack } from './IconBack';
 import { IconChevronBack } from './IconChevronBack';
 import { IconUserPlaceholder } from './IconUserPlaceholder';
 import { IconEdit } from './IconEdit';
@@ -12,11 +10,8 @@ import { IconFilters } from './IconFilters';
 import { IconClearFilters } from './IconClearFilters';
 import { IconHeart } from './IconHeart';
 import { IconHeartOutline } from './IconHeartOutline';
-import { IconCalendar } from './IconCalendar';
 import { IconEmail } from './IconEmail';
 import { IconPassword } from './IconPassword';
-import { IconDirection } from './IconDirection';
-import { IconDeparture } from './IconDeparture';
 import { IconRoute } from './IconRoute';
 import { IconCar } from './IconCar';
 import { IconEarth } from './IconEarth';
@@ -25,9 +20,7 @@ import { IconClock } from './IconClock';
 
 export {
   IconUser,
-  IconBell,
   IconBriefcase,
-  IconBack,
 	IconChevronBack,
   IconUserPlaceholder,
   IconEdit,
@@ -38,11 +31,8 @@ export {
   IconClearFilters,
   IconHeart,
   IconHeartOutline,
-  IconCalendar,
   IconEmail,
   IconPassword,
-  IconDirection,
-  IconDeparture,
   IconRoute,
   IconCar,
 	IconEarth,
